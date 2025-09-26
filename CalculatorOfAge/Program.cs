d2d08b5f3598042001db1c9a86f0b2d1d2ea50bc
Dred-limit <53877236+Dred-limit@users.noreply.github.com>
2025-09-26 16:34:13 +0300

    Add project files.

---
+++ CalculatorOfAge/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+
+class CalcalatorAge
+{
+    static void Main()
+    {
+        WriteLine("Введите свою дату рождения");
+        string userDate = ReadLine();
+
+        if (DateTime.TryParse(userDate, out DateTime birthday))
+        {
+            WriteLine($"Твоя дата рождения: {birthday:d}");
+            if (birthday > DateTime.Today)
+            {
+                WriteLine("Ошибка: Ты из будущего? Введи коректную дату" +
+                    " рождения, не позже сегодняшнего дня!");
+                return;
+            }
+        }
+        else
+        {
+            WriteLine("Ты написал неверный формат даты, попробуй: дд.мм.гггг");
+            return;
+        }
+
+        DateTime today = DateTime.Today;
+        int age = today.Year - birthday.Year;
+
+        // Но нужно учесть, был ли день рождения в этом году
+        if (today.Month < birthday.Month ||
+            (today.Month == birthday.Month &&
+            today.Day < birthday.Day))
+        {
+            age--; // День рождения еще не был в этом году
+        }
+        WriteLine($"Тебе {age} лет!");
+
+        DateTime nextBirthday = new DateTime(today.Year, birthday.Month, birthday.Day);
+        if (nextBirthday < today)
+        {
+            nextBirthday = nextBirthday.AddYears(1);
+        }
+
+        int daysUntilBirthday = (nextBirthday - today).Days;
+        WriteLine($"До следующего дня рождения: {daysUntilBirthday} дней!");
+    }
+}
